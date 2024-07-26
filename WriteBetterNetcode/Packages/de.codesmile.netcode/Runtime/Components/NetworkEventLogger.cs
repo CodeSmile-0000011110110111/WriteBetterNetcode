884bbe6f39c8626122d5b7f973b33fd7bb41571c
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-07-26 18:36:15 +0200

    NetworkEventLogger: add ticks to logs, shorten the property messages

--- WriteBetterNetcode/Packages/de.codesmile.netcode/Runtime/Components/NetworkEventLogger.cs
+++ WriteBetterNetcode/Packages/de.codesmile.netcode/Runtime/Components/NetworkEventLogger.cs
@@ -3,7 +3,6 @@
 
 using CodeSmile.Netcode.Extensions;
 using System;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEditor;
 using UnityEngine;
@@ -13,10 +12,12 @@ namespace CodeSmile.Netcode.Components
 	/// <summary>
 	///     Will log all NetworkManager events. Best to add this component on the same object as the NetworkManager.
 	/// </summary>
+	/// <remarks>
+	///     The component is only active if DEBUG or DEVELOPMENT_BUILD symbols are defined.
+	/// </remarks>
 	public class NetworkEventLogger : MonoBehaviour
 	{
 		[SerializeField] private Boolean m_LogPropertyChanges = true;
-		private readonly List<String> m_UpdateMessages = new();
 
 		private Boolean m_ListeningState;
 		private Boolean m_ShutdownInProgressState;
@@ -86,49 +87,38 @@ namespace CodeSmile.Netcode.Components
 				if (net.IsListening != m_ListeningState)
 				{
 					m_ListeningState = net.IsListening;
-					m_UpdateMessages.Add($"NetworkManager.IsListening = {net.IsListening}");
+					Log($"IsListening = {net.IsListening}");
 				}
 				if (net.ShutdownInProgress != m_ShutdownInProgressState)
 				{
 					m_ShutdownInProgressState = net.ShutdownInProgress;
-					m_UpdateMessages.Add($"NetworkManager.ShutdownInProgress = {net.ShutdownInProgress}");
+					Log($"ShutdownInProgress = {net.ShutdownInProgress}");
 				}
 				if (net.IsClient != m_IsClientState)
 				{
 					m_IsClientState = net.IsClient;
-					m_UpdateMessages.Add($"NetworkManager.IsClient = {net.IsClient}");
-				}
-				if (net.IsConnectedClient != m_IsConnectedClientState)
-				{
-					m_IsConnectedClientState = net.IsConnectedClient;
-					m_UpdateMessages.Add($"NetworkManager.IsConnectedClient = {net.IsConnectedClient}");
-				}
-				if (net.IsApproved != m_IsApprovedState)
-				{
-					m_IsApprovedState = net.IsApproved;
-					m_UpdateMessages.Add($"NetworkManager.IsApproved = {net.IsApproved}");
+					Log($"IsClient = {net.IsClient}");
 				}
 				if (net.IsHost != m_IsHostState)
 				{
 					m_IsHostState = net.IsHost;
-					m_UpdateMessages.Add($"NetworkManager.IsHost = {net.IsHost}");
+					Log($"IsHost = {net.IsHost}");
 				}
 				if (net.IsServer != m_IsServerState)
 				{
 					m_IsServerState = net.IsServer;
-					m_UpdateMessages.Add($"NetworkManager.IsServer = {net.IsServer}");
+					Log($"IsServer = {net.IsServer}");
 				}
-
-				LogAndClearUpdateMessages();
-			}
-		}
-
-		private void LogAndClearUpdateMessages()
-		{
-			if (m_UpdateMessages.Count > 0)
-			{
-				Log($"Property changes: {String.Join("; ", m_UpdateMessages)}");
-				m_UpdateMessages.Clear();
+				if (net.IsApproved != m_IsApprovedState)
+				{
+					m_IsApprovedState = net.IsApproved;
+					Log($"IsApproved = {net.IsApproved}");
+				}
+				if (net.IsConnectedClient != m_IsConnectedClientState)
+				{
+					m_IsConnectedClientState = net.IsConnectedClient;
+					Log($"IsConnectedClient = {net.IsConnectedClient}");
+				}
 			}
 		}
 
@@ -141,13 +131,27 @@ namespace CodeSmile.Netcode.Components
 		private void OnClientDisconnect(UInt64 clientId) => Log($"OnClientDisconnect(clientId: {clientId})");
 
 		private void OnConnectionEvent(NetworkManager net, ConnectionEventData data) => Log(
-			$"OnConnectionEvent(NetworkManager: {net}, ConnectionEvent: {data.EventType} for clientId: {data.ClientId})");
+			$"OnConnectionEvent({data.EventType}: ClientId = {data.ClientId})");
 
 		private void OnTransportFailure() => Log("OnTransportFailure()");
 
 		private void OnSessionOwnerPromoted(UInt64 sessionownerpromoted) =>
 			Log($"OnSessionOwnerPromoted(sessionownerpromoted: {sessionownerpromoted})");
 
-		private void Log(String message) => Debug.Log($"[#{Time.frameCount}: NetworkEvent] {message}");
+		private void Log(String message)
+		{
+			var localTick = -1;
+			var serverTick = -1;
+			var frameCount = Time.frameCount;
+
+			var net = NetworkManager.Singleton;
+			if (net)
+			{
+				localTick = net.LocalTime.Tick;
+				serverTick = net.ServerTime.Tick;
+			}
+
+			Debug.Log($"[LT:{localTick}|ST:{serverTick}|FC:{frameCount}] {message}");
+		}
 	}
 }
