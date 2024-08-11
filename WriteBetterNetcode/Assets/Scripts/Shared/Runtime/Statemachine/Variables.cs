5efe27a298bb41af7f8e2cc5918bd4363c331af9
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-08-11 12:11:44 +0200

    SM: fix Var<T> all having the same name 'T'

--- WriteBetterNetcode/Assets/Scripts/Shared/Runtime/Statemachine/Variables.cs
+++ WriteBetterNetcode/Assets/Scripts/Shared/Runtime/Statemachine/Variables.cs
@@ -40,7 +40,7 @@ namespace CodeSmile.Statemachine
 
 		public FloatVar GetFloat(String name) => m_Variables[name] as FloatVar;
 
-		public Var<T> DefineVar<T>() where T : struct => AddVariable(nameof(T), new Var<T>());
+		public Var<T> DefineVar<T>() where T : struct => AddVariable(typeof(T).Name, new Var<T>());
 		public Var<T> DefineVar<T>(String name, T value = default) where T : struct => AddVariable(name, new Var<T>(value));
 
 		public Var<T> GetVar<T>(String name) where T : struct => m_Variables[name] as Var<T>;
