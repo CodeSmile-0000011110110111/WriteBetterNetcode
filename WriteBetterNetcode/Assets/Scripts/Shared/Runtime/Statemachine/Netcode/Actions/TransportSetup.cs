983eda0a1d81026ae161d3fc95f68867ec6d2dd1
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-08-11 16:27:54 +0200

    TransportSetup: fix connectionType cannot be null (must be "udp")

--- WriteBetterNetcode/Assets/Scripts/Shared/Runtime/Statemachine/Netcode/Actions/TransportSetup.cs
+++ WriteBetterNetcode/Assets/Scripts/Shared/Runtime/Statemachine/Netcode/Actions/TransportSetup.cs
@@ -38,7 +38,7 @@ namespace CodeSmile.Statemachine.Netcode.Actions
 			transport.UseEncryption = transportConfig.UseEncryption;
 			transport.UseWebSockets = transportConfig.UseWebSockets;
 
-			var connectionType = transportConfig.UseEncryption ? transportConfig.UseWebSockets ? "wss" : "dtls" : null;
+			var connectionType = transportConfig.UseEncryption ? transportConfig.UseWebSockets ? "wss" : "dtls" : "udp";
 
 			// TODO: transport encryption ... set secrets and such
 			if (transportConfig.UseEncryption)
