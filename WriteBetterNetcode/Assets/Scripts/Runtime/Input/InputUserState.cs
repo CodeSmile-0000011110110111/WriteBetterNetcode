aa211129f4cc6e5a04442dcdcdcba26ce20ca9c6
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-08-23 12:57:18 +0200

    code cleanup

--- WriteBetterNetcode/Assets/Scripts/Runtime/Input/InputUserState.cs
+++ WriteBetterNetcode/Assets/Scripts/Runtime/Input/InputUserState.cs
@@ -18,18 +18,12 @@ namespace CodeSmile
 		public event Action<InputUser, InputDevice> OnDevicePaired;
 		public event Action<InputUser, InputDevice> OnDeviceUnpaired;
 
-		//[SerializeField] private InputActionAsset m_Actions;
-
 		private readonly InputUser[] m_Users = new InputUser[Constants.MaxCouchPlayers];
-		//private readonly InputActionMap[] m_ActiveActionMaps = new InputActionMap[Constants.MaxCouchPlayers];
-		//private readonly Action<InputAction.CallbackContext>[] m_ActionTriggeredDelegates = new Action<InputAction.CallbackContext>[Constants.MaxCouchPlayers];
-		//private Object m_OnJoinSessionActions;
-
 		private Boolean m_UserDevicePairingEnabled = true;
-
 		private GeneratedInputActions m_InputActions;
 
 		private InputUser HostUser { get => m_Users[0]; set => m_Users[0] = value; }
+		
 		public Boolean UserDevicePairingEnabled
 		{
 			get => m_UserDevicePairingEnabled;
@@ -54,8 +48,6 @@ namespace CodeSmile
 
 		private void Awake()
 		{
-			//if (m_Actions == null) throw new MissingReferenceException("Input Actions not assigned");
-
 			CreateInputUsers();
 
 			InputSystem.onDeviceChange += OnInputDeviceChange;
@@ -79,8 +71,6 @@ namespace CodeSmile
 
 		private void OnDisable() => SetPairingActionsEnabled(false);
 
-		private void Start() {}
-
 		private void TryPairUserDevice(InputDevice device)
 		{
 			// only binding to gamepads supported, keyboard/mouse always bound to host player
@@ -103,8 +93,6 @@ namespace CodeSmile
 					m_Users[userIndex] = user;
 
 					OnDevicePaired?.Invoke(user, device);
-
-					//Debug.Log($"paired to {m_Users[userIndex]}: {device.name}");
 				}
 			}
 		}
@@ -127,8 +115,6 @@ namespace CodeSmile
 				HostUser = InputUser.PerformPairingWithDevice(device, HostUser);
 
 				OnDeviceUnpaired?.Invoke(deviceUser.Value, device);
-
-				//Debug.Log($"unpaired from {deviceUser}: {device.name}");
 			}
 		}
 
@@ -177,36 +163,10 @@ namespace CodeSmile
 			Debug.Assert(HostUser.index == 0);
 		}
 
-		private void AssignActionTriggeredDelegates()
-		{
-			// m_ActionTriggeredDelegates[0] = OnActionTriggered_User0;
-			// m_ActionTriggeredDelegates[1] = OnActionTriggered_User1;
-			// m_ActionTriggeredDelegates[2] = OnActionTriggered_User2;
-			// m_ActionTriggeredDelegates[3] = OnActionTriggered_User3;
-		}
-
-		private void SwitchActionMap(InputUser user, InputActionMap actionMap)
-		{
-			// var currentActionMap = m_ActiveActionMaps[user.index];
-			// if (currentActionMap != null)
-			// 	currentActionMap.actionTriggered -= m_ActionTriggeredDelegates[user.index];
-			//
-			// actionMap.actionTriggered += m_ActionTriggeredDelegates[user.index];
-			// m_ActiveActionMaps[user.index] = actionMap;
-		}
-
-		private void OnActionTriggered_User0(InputAction.CallbackContext ctx) => OnActionTriggered(0, ctx);
-		private void OnActionTriggered_User1(InputAction.CallbackContext ctx) => OnActionTriggered(1, ctx);
-		private void OnActionTriggered_User2(InputAction.CallbackContext ctx) => OnActionTriggered(2, ctx);
-		private void OnActionTriggered_User3(InputAction.CallbackContext ctx) => OnActionTriggered(3, ctx);
-
 		private void OnActionTriggered(Int32 playerIndex, InputAction.CallbackContext ctx) =>
 			Debug.Log($"User #{playerIndex}: {ctx.action.name} {ctx.phase}");
 
-		private void OnInputActionChange(Object action, InputActionChange change)
-		{
-			//Debug.Log($"{change}: {action}");
-		}
+		private void OnInputActionChange(Object action, InputActionChange change) => Debug.Log($"{change}: {action}");
 
 		private void OnInputUserChange(InputUser user, InputUserChange change, InputDevice device) =>
 			Debug.Log($"USER {change}: {user} - {device}");
@@ -219,13 +179,11 @@ namespace CodeSmile
 				case InputDeviceChange.Reconnected:
 				case InputDeviceChange.Enabled:
 					Debug.Log($"DEVICE {change}: {device}");
-					OnInputDeviceAdded(device);
 					break;
 				case InputDeviceChange.Removed:
 				case InputDeviceChange.Disconnected:
 				case InputDeviceChange.Disabled:
 					Debug.Log($"DEVICE {change}: {device}");
-					OnInputDeviceRemoved(device);
 					break;
 				case InputDeviceChange.UsageChanged:
 					Debug.Log($"DEVICE {change}: {device}");
@@ -239,110 +197,5 @@ namespace CodeSmile
 					break;
 			}
 		}
-
-		private void OnInputDeviceAdded(InputDevice device)
-		{
-			// var playerIndex = GetInactivePlayerIndex();
-			// if (playerIndex >= 0)
-			// {
-			// 	//SetPlayerInputActive(playerIndex, true);
-			// 	LogInputDevicePairing();
-			// }
-		}
-
-		private void OnInputDeviceRemoved(InputDevice device)
-		{
-			// var playerIndex = GetPlayerIndexForDevice(device);
-			// if (playerIndex >= 0)
-			// {
-			// 	SetPlayerInputActive(playerIndex, false);
-			// 	LogInputDevicePairing();
-			// }
-		}
-
-		/*
-		[SerializeField] private PlayerInput[] m_PlayersInput = new PlayerInput[Constants.MaxCouchPlayers];
-
-		private void PairHostPlayerWithDevices()
-		{
-			// Host player pairs with all connected devices
-			var didPairMouseKeyboard = false;
-			var didPairGamepadJoystick = false;
-
-			var devices = InputSystem.devices;
-			for (var deviceIndex = 0; deviceIndex < devices.Count; deviceIndex++)
-			{
-				var device = devices[deviceIndex];
-				if (device != null)
-				{
-					Debug.Log($"{device}");
-
-					didPairMouseKeyboard = device is Mouse || device is Keyboard || didPairMouseKeyboard;
-					didPairGamepadJoystick = device is Gamepad || device is Joystick || didPairGamepadJoystick;
-
-					SetPlayerInputActive(0, true);
-
-					if (didPairMouseKeyboard && didPairGamepadJoystick)
-					{
-						break;
-					}
-				}
-			}
-		}
-
-		private Boolean IsKeyboardOrMouse(InputDevice device) =>
-			device == Keyboard.current.device || device == Mouse.current.device;
-
-		private void SetAllPlayerInputsActive(Boolean active)
-		{
-			foreach (var playerInput in m_PlayersInput)
-				playerInput.gameObject.SetActive(active);
-		}
-
-		private void SetPlayerInputActive(Int32 playerIndex, Boolean active)
-		{
-			Debug.Log($"SetPlayerInputActive({playerIndex}, active={active})");
-			m_PlayersInput[playerIndex].gameObject.SetActive(active);
-		}
-
-		private Int32 GetInactivePlayerIndex()
-		{
-			for (var playerIndex = 0; playerIndex < Constants.MaxCouchPlayers; playerIndex++)
-			{
-				var playerInput = m_PlayersInput[playerIndex];
-				if (playerInput.gameObject.activeSelf == false)
-					return playerIndex;
-			}
-
-			return -1;
-		}
-
-		private Int32 GetPlayerIndexForDevice(InputDevice device)
-		{
-			for (var playerIndex = 0; playerIndex < Constants.MaxCouchPlayers; playerIndex++)
-			{
-				var playerInput = m_PlayersInput[playerIndex];
-				if (playerInput.devices.Contains(device))
-					return playerIndex;
-			}
-
-			return -1;
-		}
-
-		internal static void LogInputDevicePairing()
-		{
-			var devices = InputSystem.devices;
-			for (var i = 0; i < devices.Count; i++)
-			{
-				var device = devices[i];
-				if (device != null)
-				{
-					var pairedInput = PlayerInput.FindFirstPairedToDevice(device);
-					if (pairedInput != null)
-						Debug.Log($"{pairedInput.name} paired with {device.name}");
-				}
-			}
-		}
-		*/
 	}
 }
