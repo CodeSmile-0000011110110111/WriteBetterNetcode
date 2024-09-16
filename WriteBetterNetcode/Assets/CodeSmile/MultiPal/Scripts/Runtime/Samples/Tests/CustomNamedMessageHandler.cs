dab67602f38a0c6771474d89ac0c20bb20bb4ef0
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-09-16 17:53:09 +0200

    test custom messaging events

--- WriteBetterNetcode/Assets/CodeSmile/MultiPal/Scripts/Runtime/Samples/Tests/CustomNamedMessageHandler.cs
+++ WriteBetterNetcode/Assets/CodeSmile/MultiPal/Scripts/Runtime/Samples/Tests/CustomNamedMessageHandler.cs
@@ -9,82 +9,73 @@ using UnityEngine;
 
 namespace CodeSmile.MultiPal.Samples.Tests
 {
-	public class CustomNamedMessageHandler : NetworkBehaviour
+	public class CustomNamedMessageHandler : MonoBehaviour
 	{
-		[Tooltip("The name identifier used for this custom message handler.")]
-		public String kMessageName = "MyCustomNamedMessage";
+		private readonly String kMessageName = nameof(CustomNamedMessageHandler);
 
-		/// <summary>
-		///     For most cases, you want to register once your NetworkBehaviour's
-		///     NetworkObject (typically in-scene placed) is spawned.
-		/// </summary>
-		public override void OnNetworkSpawn()
+		private NetworkManager NetworkManager => NetworkManager.Singleton;
+		private bool IsServer => NetworkManager.Singleton.IsServer;
+
+		//public override void OnNetworkSpawn()
+		void OnEnable()
 		{
-			// Both the server-host and client(s) register the custom named message.
-			NetworkManager.CustomMessagingManager.RegisterNamedMessageHandler(kMessageName, ReceiveMessage);
+			NetworkManager.CustomMessagingManager.RegisterNamedMessageHandler(kMessageName, ReceiveMessageInternal);
 
 			if (IsServer)
-			{
-				// Server broadcasts to all clients when a new client connects (just for example purposes)
 				NetworkManager.OnClientConnectedCallback += OnClientConnectedCallback;
-			}
 			else
 			{
-				// Clients send a unique Guid to the server
 				var guid = Guid.NewGuid();
 				Debug.Log($"<color=yellow>Client sending GUID ({guid}) to server.</color>");
 				SendMessage(guid);
 			}
 		}
 
+		//public override void OnNetworkDespawn()
+		void OnDisable()
+		{
+			if (NetworkManager != null)
+			{
+				NetworkManager.CustomMessagingManager?.UnregisterNamedMessageHandler(kMessageName);
+				NetworkManager.OnClientDisconnectCallback -= OnClientConnectedCallback;
+			}
+		}
+
 		private void OnClientConnectedCallback(UInt64 obj) => SendMessage(Guid.NewGuid());
 
-		public override void OnNetworkDespawn()
-		{
-			// De-register when the associated NetworkObject is despawned.
-			NetworkManager.CustomMessagingManager?.UnregisterNamedMessageHandler(kMessageName);
-			// Whether server or not, unregister this.
-			NetworkManager.OnClientDisconnectCallback -= OnClientConnectedCallback;
-		}
-
-		/// <summary>
-		///     Invoked when a custom message of type <see cref="kMessageName" />
-		/// </summary>
-		private void ReceiveMessage(UInt64 senderId, FastBufferReader messagePayload)
-		{
-			var receivedMessageContent = new ForceNetworkSerializeByMemcpy<Guid>(new Guid());
-			messagePayload.ReadValueSafe(out receivedMessageContent);
-			if (IsServer)
-				Debug.Log($"<color=red>Sever received GUID ({receivedMessageContent.Value}) from client ({senderId})</color>");
-			else
-				Debug.Log($"<color=red>Client received GUID ({receivedMessageContent.Value}) from the server.</color>");
-		}
-
-		/// <summary>
-		///     Invoke this with a Guid by a client or server-host to send a
-		///     custom named message.
-		/// </summary>
 		public void SendMessage(Guid inGameIdentifier)
 		{
-			var messageContent = new ForceNetworkSerializeByMemcpy<Guid>(inGameIdentifier);
-			var writer = new FastBufferWriter(1100, Allocator.Temp);
-			var customMessagingManager = NetworkManager.CustomMessagingManager;
-			using (writer)
-			{
-				writer.WriteValueSafe(messageContent);
-				if (IsServer)
-				{
-					// This is a server-only method that will broadcast the named message.
-					// Caution: Invoking this method on a client will throw an exception!
-					customMessagingManager.SendNamedMessageToAll(kMessageName, writer);
-				}
-				else
-				{
-					// This is a client or server method that sends a named message to one target destination
-					// (client to server or server to client)
-					customMessagingManager.SendNamedMessage(kMessageName, NetworkManager.ServerClientId, writer);
-				}
-			}
+			var content = new ForceNetworkSerializeByMemcpy<Guid>(inGameIdentifier);
+			using var payload = new FastBufferWriter(1100, Allocator.Temp);
+			payload.WriteValueSafe(content);
+
+			SendMessageInternal(payload);
+		}
+
+		private void SendMessageInternal(FastBufferWriter payload)
+		{
+			var msgManager = NetworkManager.CustomMessagingManager;
+
+			if (IsServer)
+				msgManager.SendNamedMessageToAll(kMessageName, payload);
+			else
+				msgManager.SendNamedMessage(kMessageName, NetworkManager.ServerClientId, payload);
+		}
+
+		private void ReceiveMessageInternal(UInt64 senderId, FastBufferReader payload)
+		{
+			var content = new ForceNetworkSerializeByMemcpy<Guid>(new Guid());
+			payload.ReadValueSafe(out content);
+
+			OnMessageReceived(senderId, content);
+		}
+
+		private void OnMessageReceived(UInt64 senderId, ForceNetworkSerializeByMemcpy<Guid> content)
+		{
+			if (IsServer)
+				Debug.Log($"<color=red>Sever received GUID ({content.Value}) from client ({senderId})</color>");
+			else
+				Debug.Log($"<color=red>Client received GUID ({content.Value}) from the server.</color>");
 		}
 	}
 }
